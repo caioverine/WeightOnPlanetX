f139f7024c2bac7624a225a81f1900ea84c5958c
Caio Vinícius <viniciuscaio.17@gmail.com>
2019-01-23 16:02:22 -0200

    first commit

---
+++ lib/ui/home.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    
+    return HomeState();
+  }
+}
+    
+class HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Weight on Planet X"),
+        centerTitle: true,
+        backgroundColor: Colors.black38
+      ),
+      backgroundColor: Colors.blueGrey,
+      body: Container(
+        alignment: Alignment.topCenter,
+        child: ListView(
+          padding: const EdgeInsets.all(2.5),
+          children: <Widget>[
+            Image.asset('images/planet.png',
+              height: 133.0,
+              width: 200.0,
+            ),
+            Container(
+              margin: const EdgeInsets.all(3.0),
+              alignment: Alignment.center,
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    controller: null,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Your Weight on Earth',
+                      hintText: 'In Pounds',
+                      icon: Icon(Icons.person_outline)
+                    )
+                  )
+                ]
+              )
+            )
+          ]
+        )
+      )
+    );
+  }
+}
