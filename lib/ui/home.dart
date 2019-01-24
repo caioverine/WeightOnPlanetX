3b04847f3bcc14fc1f7ac4ed21f7555c8428bcd3
Caio Vinícius <viniciuscaio.17@gmail.com>
2019-01-24 18:40:09 -0200

    Completed application

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -9,6 +9,12 @@ class Home extends StatefulWidget{
 }
     
 class HomeState extends State<Home> {
+
+  final TextEditingController _weightController = new TextEditingController();
+  int radioValue = 0;
+  double _finalResult = 0.0;
+  String _formattedText = "";
+
   @override
   Widget build(BuildContext context) {
     
@@ -34,12 +40,65 @@ class HomeState extends State<Home> {
               child: Column(
                 children: <Widget>[
                   TextField(
-                    controller: null,
+                    controller: _weightController,
                     keyboardType: TextInputType.number,
                     decoration: InputDecoration(
                       labelText: 'Your Weight on Earth',
                       hintText: 'In Pounds',
                       icon: Icon(Icons.person_outline)
+                    )
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(5.0)
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Radio<int>(
+                        value: 0,
+                        groupValue: radioValue,
+                        onChanged: handleRadioValueChanged,
+                        activeColor: Colors.brown
+                      ),
+                      Text("Pluto",
+                        style: TextStyle(
+                          color: Colors.white30
+                        )
+                      ),
+
+                      Radio<int>(
+                        value: 1,
+                        groupValue: radioValue,
+                        onChanged: handleRadioValueChanged,
+                        activeColor: Colors.red
+                      ),
+                      Text("Mars",
+                        style: TextStyle(
+                          color: Colors.white30
+                        )
+                      ),
+
+                      Radio<int>(
+                        value: 2,
+                        groupValue: radioValue,
+                        onChanged: handleRadioValueChanged,
+                        activeColor: Colors.orangeAccent,
+                      ),
+                      Text("Venus",
+                        style: TextStyle(
+                          color: Colors.white30
+                        )
+                      )
+                    ]
+                  ),
+
+                  Padding(padding: EdgeInsets.all(15.6)),
+
+                  Text(_weightController.text.isNotEmpty ? "$_formattedText lbs" : "Plese enter your weight",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 19.4,
+                      fontWeight: FontWeight.w500
                     )
                   )
                 ]
@@ -49,5 +108,41 @@ class HomeState extends State<Home> {
         )
       )
     );
+  }
+
+  void handleRadioValueChanged(int value) {
+
+    setState(() {
+
+      radioValue = value;
+      
+      switch(radioValue) {
+        case 0: //Pluto
+          _finalResult = calculateWeight(_weightController.text, 0.06); // 0.06 is value for Pluto
+          _formattedText = "Your weight on Pluto is ${_finalResult.toStringAsFixed(1)}";
+          break;
+        case 1: //Mars
+          _finalResult = calculateWeight(_weightController.text, 0.38); // 0.38 is value for Mars
+          _formattedText = "Your weight on Mars is ${_finalResult.toStringAsFixed(1)}";
+          break;
+        case 2: //Venus
+          _finalResult = calculateWeight(_weightController.text, 0.91); // 0.91 is value for Venus
+          _formattedText = "Your weight on Venus is ${_finalResult.toStringAsFixed(1)}";
+          break;
+      }
+    });
+  }
+
+  double calculateWeight(String earthWeight, double planetValue){
+
+    if(earthWeight.isNotEmpty && int.parse(earthWeight) > 0) {
+
+      return int.parse(earthWeight) * planetValue;
+    } else {
+
+      print("wrong!");
+
+      return int.parse(earthWeight) * 180.0;
+    }
   }
 }
